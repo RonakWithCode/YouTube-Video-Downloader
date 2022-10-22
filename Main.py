209cab2d5cacbf33fc968eeb5508fd8f99be85fa
Ronak Jain <98526174+RonakWithCode@users.noreply.github.com>
2022-10-22 11:25:10 +0530

    Add a select save video location

--- Main.py
+++ Main.py
@@ -1,10 +1,16 @@
 import tkinter as tk
+from tkinter import filedialog
 from pytube import YouTube
-def Download_Video():     
+import os
+u_name = os.getlogin()
+def Download_Video():
     url = YouTube(str(link.get()))
     video = url.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
     try:
-        video.download("C:\\Users\\ronak\\Videos\\YourTube-video-Downloader\\")
+        a =filedialog.askdirectory() 
+        if a=="":
+            a = f"C:\\Users\\{u_name}\\Videos\\YourTube-video-Downloader\\"   
+        video.download(a)
         tk.Label(window, text = 'Your Video is downloaded!', font = 'arial 15',fg="White",bg="#EC7063").place(x= 10 , y = 140)  
     except Exception as e:
         print("error ")
@@ -13,17 +19,15 @@ def Download_Video():
         print("error ")
         print("error ")
         print(e)
-        
+
 window = tk.Tk()
-window.geometry("600x200")
+window.geometry("600x400")
 window.config(bg="#EC7063")
 window.resizable(width=False,height=False)
 window.title('YouTube Video Downloader')
- 
 link = tk.StringVar()
-tk.Label(window,text = '                   Youtube Video Downloader                    ', font ='arial 20 bold',fg="White",bg="Black").pack()
+tk.Label(window,text = '                   Youtube Video Downloader                    ', font ='arial 20 bold',fg="White",bg="#EC7063").pack()
 tk.Label(window, text = 'Enter the link of youtube video ', font = 'arial 20 bold',fg="Black",bg="#EC7063").place(x= 5 , y = 60)
 link_enter = tk.Entry(window, width = 53,textvariable = link,font = 'arial 15 bold',bg="lightgreen").place(x = 5, y = 100)
-tk.Button(window,text = 'DOWNLOAD', font = 'arial 15 bold' ,fg="white",bg = 'black', padx = 2,command=Download_Video).place(x=385 ,y = 140)
- 
+tk.Button(window,text = 'DOWNLOAD', font = 'arial 15 bold' ,fg="white",bg = 'black', padx = 2,command=Download_Video).place(x=400 ,y = 142)
 window.mainloop()
